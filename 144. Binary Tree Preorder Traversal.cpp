7532a0980c332dddf80d04b0076ee5fc3e93c510
Rongyu Franklin Lin <lryfranklinasu@gmail.com>
2016-09-11 15:53:58 -0700

    No. 144 Finished

---
+++ 144. Binary Tree Preorder Traversal.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+        preorder(root, result);
+        return result;
+    }
+    
+    void preorder(TreeNode *root, vector<int> &result){
+        if (!root) return;
+        result.push_back(root->val);
+        preorder(root->left, result);
+        preorder(root->right, result);
+    }
+};
